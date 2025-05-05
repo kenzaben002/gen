fb845f7346bf185d00ba0478c291e437ecad35eb
kenzaben002 <kenzobenhammaali@gmail.com>
2025-05-05 11:27:50 +0200

    Update h12_train.py

--- RL/h12_train.py
+++ RL/h12_train.py
@@ -182,7 +182,7 @@ def get_cfgs():
         "termination_if_roll_greater_than": 10,  # degree
         "termination_if_pitch_greater_than": 10,
         # base pose
-        "base_init_pos": [0.0, 0.0, 1.05],
+        "base_init_pos": [0.0, 0.0, 1.04],
         "base_init_quat": [1.0, 0.0, 0.0, 0.0],
         "episode_length_s": 20.0,
         "resampling_time_s": 4.0,
@@ -201,15 +201,15 @@ def get_cfgs():
     }
     reward_cfg = {
         "tracking_sigma": 0.25,
-        "base_height_target": 1.05,
-        "feet_height_target": 0.00075,
+        "base_height_target": 1.04,
+        "feet_height_target": 0.0,
         "reward_scales": {
             "tracking_lin_vel": 1.0,
             "tracking_ang_vel": 0.2,
-            "lin_vel_z": -1.5,
-            "base_height": -5.0,
+            "lin_vel_z": -10,
+            "base_height": -50.0,
             "action_rate": -0.005,
-            "similar_to_default": -0.1,
+            "similar_to_default": -0.0,
         },
     }
     command_cfg = {
